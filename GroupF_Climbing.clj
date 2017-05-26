a91de379fdcb594bcc408dcbbb3fa231447388b2
vonswirl <vonswirl@googlemail.com>
2017-05-26 04:03:22 +0100

    dont merge as this will override the originl version

--- GroupF_Climbing.clj
+++ GroupF_Climbing.clj
@@ -4,124 +4,148 @@
 ;(require '[cgsx.tools.opertator_search.states :refer :all])
 ;(require '[operator-search.planner :refer :all])
 
-(def world-state-1 ;Agent is on the floor
+(def world-state-1                                          ;World State = agent and box both on floor
   '#{(Agent agent)
-     ;(isa object platform)
      (isa obj box)
      (manipulable box)
      (isa location platform1)
      (isa location platform2)
      (isa location floor)
-     ;(isa location box) ;This can be commented out
      (climbable platform1)
      (climbable platform2)
-     ;(climbable box) ;Take this out too
-     (holds nil agent) ;Change this to box for different searches
-     (at floor agent) ;Changed this from (at floor agent)
+     (holds nil agent)
+     (at floor agent)
      (at floor platform1)
      (at floor platform2)
-     (on floor box) ;Originally on floor box
+     (on floor box)
      })
 
-(def world-state-2 ;Agent on top of platform
+(def world-state-2                                          ;World State = agent on platform2 box on platform1
   '#{(Agent agent)
      (isa obj box)
      (manipulable box)
      (isa location platform1)
      (isa location platform2)
      (isa location floor)
-     (climbed platform1)
-     (climbable platform2)
-     (holds nil agent) ;Change this to box for different searches
-     (on-top platform1 agent) ;Changed this from (at floor agent)
+     (climbable platform1)
+     (climbed platform2)
+     (holds nil agent)
+     (on-top platform2 agent)
+     (at platform2 agent)
      (at floor platform1)
      (at floor platform2)
-     (on-top platform1 box) ;Originally on floor box (on ?location box)
+     (on platform1 box)
      })
 
-;(ops-search world-state-1 '((holds box agent)) ops)            Get agent to hold box
-;(ops-search world-state-1 '((at platform1 agent)) ops)         Move an agent to the platform
-;(ops-search world-state-1 '((next-to platform1 agent)) ops)    Also moves
-;(ops-search world-state-1 '((on-top platform1 agent)) ops)     Climbs ontop of platform
-;(ops-search world-state-2 '((at floor agent)) ops)             Takes agent from platform onto floor (in world (on-top platform1 agent))
-;(ops-search world-state-2 '((holds box agent)) ops)            Tries to take box off platform with agent stood on it
+;(ops-search world-state-1 '((holds box agent)) ops)            Get agent to hold box...
+;(ops-search world-state-1 '((next-to platform1 agent)) ops)    Agent next-to p1...
+;(ops-search world-state-1 '((next-to platform2 agent)) ops)    Agent next-to p2...
+;(ops-search world-state-1 '((on-top platform1 agent)) ops)     Agent on p1...
+;(ops-search world-state-1 '((on-top platform2 agent)) ops)     Agent on p2...
+;(ops-search world-state-1 '((on platform1 box)) ops)           Box on p1...
+;(ops-search world-state-1 '((on platform2 box)) ops)           Box on p2...
+;(ops-search world-state-1 '((on floor box)) ops)               Box on floor...
+;(ops-search world-state-1 '((at floor agent)) ops)             Agent on floor...
+
+;(ops-search world-state-2 '((holds box agent)) ops)            Get agent to hold box...
+;(ops-search world-state-2 '((next-to platform1 agent)) ops)    Agent next-to p1...
+;(ops-search world-state-2 '((next-to platform2 agent)) ops)    Agent next-to p2...
+;(ops-search world-state-2 '((on-top platform1 agent)) ops)     Agent on p1...
+;(ops-search world-state-2 '((on-top platform2 agent)) ops)     Agent on p2...
+;(ops-search world-state-2 '((on platform1 box)) ops)           Box on p1...
+;(ops-search world-state-2 '((on platform2 box)) ops)           Box on p2...
+;(ops-search world-state-2 '((on floor box)) ops)               Box on floor...
+;(ops-search world-state-2 '((at floor agent)) ops)             Agent on floor...
 
 (def ops
   '{move
     {:pre
-          ( (at ?location1 agent)
+          ((at floor agent)
+        ;    (next-to ?location1 agent)
             (isa location ?location2)
             (at floor ?location2))
-     :del ( (at ?location1 agent))
-     :add ( (next-to ?location2 agent))
-     :txt   (move agent from ?location1 next to ?location2)
+     :del ((next-to ?location1 agent))
+     :add ((next-to ?location2 agent))
+     :txt (Agent moved across floor to ?location2)
      }
-    pickup
-    {:pre
-          ( (holds nil agent)
-            (at ?location agent)
-            (on ?location ?obj)
-            (manipulable ?obj))
-     :del ( (on ?location ?obj)
-            (holds nil agent))
-     :add ( (holds ?obj agent))
-     :txt   (pick-up ?obj at ?location)
-    }
-    drop ;Drops onto floor only
-    {:pre
-          ( (at floor agent)
-            (holds ?obj agent)
-            (:not (holds nil agent)))
-     :del ( (holds ?obj agent))
-     :add ( (holds nil agent)
-            (on floor ?obj))
-     :txt   (drop ?obj on ?location)
-  }
+
     climb-on
     {:pre
-          ( (next-to ?location agent)  ;Need to figure out the syntax to get an agent to climb on
-            (climbable ?location))     ;First needs to move then be next-to
-     :del ( (next-to ?location agent)) ;From there the climb on can be used so in the search does it need to be (on ?location agent)
-     :add ( (on-top ?location agent)
+          ((next-to ?location agent)                        ;Need to figure out the syntax to get an agent to climb on
+            (climbable ?location)
+            (at floor agent))                               ;First needs to move then be next-to
+     :del ((next-to ?location agent)
+            (climbable ?location)
+            (at floor agent))                               ;From there the climb on can be used so in the search does it need to be (on ?location agent)
+     :add ((on-top ?location agent)
             (climbed ?location))
-     :txt   (climb-on agent on top of ?location)
-   }
+     :txt (Agent climbed ?location)
+     }
+
     climb-off
     {:pre
-          ( (on-top ?location1 agent)
-            (isa location ?location2)
+          ((on-top ?location agent)
             (climbed ?location))
-     :del ( (on-top ?location1 agent)
-            (climbed ?location))
-     :add ( (at ?location2 agent)
-            (next-to ?location1 agent)
-            (climbable ?location)) ;Platform
-     :txt (climb-off ?location1 to ?location2)
-   }
-    pick-off ;Drops onto platform only
-    {:pre
-          ( (holds nil agent)
-            (on-top ?location agent)
+     :del ((on-top ?location agent)
             (climbed ?location)
-            (on-top ?location ?obj)
-            (manipulable ?obj)
-            )
-     :del ( (holds nil agent)
-            (on-top ?location ?obj)) ;(:not (on ?location ?obj)) wat do
-     :add ( (holds ?obj ?agent))
-     :txt   (pick-off ?obj from ?location)
+            (at ?location agent))
+     :add ((at floor agent)
+            (next-to ?location agent)
+            (climbable ?location))                          ;Platform
+     :txt (Agent climbed-off ?location onto floor)
      }
-    drop-on ;This one needs looking at again
+
+    pickup
+    {:pre
+          ((holds nil agent)
+            (at ?location1 agent)
+            (on ?location1 ?obj)
+            (manipulable ?obj))
+     :del ((on ?location1 ?obj)
+            (holds nil agent))
+     :add ((holds ?obj agent))
+     :txt (Agent picked-up ?obj from ?location1)
+     }
+    pick-off                                                ;Drops onto platform only
     {:pre
           (
-            (on ?location agent) ;Set its location to be the current location
-            (isa platform ?platform)
-            (:not (on ?platform ?obj))
-            (hold ?obj ?agent))
-     :del ((:not (on ?platform ?obj))
-            (hold ?obj ?agent))
-     :add ((holds nil ?agent)
-            (on ?platform ?obj))}
-    })
+            (holds nil agent)
+            (on-top ?location agent)
+            ;(climbed ?location)
+            (on ?location ?obj)
+            (manipulable ?obj))
+     :del (
+            (holds nil agent)
+            (on ?location ?obj))                            ;(:not (on ?location ?obj)) wat do
+     :add (
+            (holds ?obj agent))
+     :txt (Agent picked-off ?obj from ?location)
+     }
+    drop                                                    ;Drops onto floor only
+    {:pre
+          (
+            (at floor agent)
+            (holds ?obj agent)
+            (:not (holds nil agent)))
+     :del (
+            (holds ?obj agent))
+     :add (
+            (holds nil agent)
+            (on floor ?obj))
+     :txt (Agent dropped ?obj onto floor)
+     }
 
-
+    drop-on                                                 ;This one needs looking at again
+    {:pre
+          (
+            (on-top ?location agent)                        ;Set its location to be the current location
+            (holds ?obj agent)
+            (:not (holds nil agent)))
+     :del (
+            ;(:not (on ?location ?obj))
+            (holds ?obj agent))
+     :add (
+            (holds nil agent)
+            (on ?location ?obj))
+     :txt (Agent dropped ?obj onto ?location)
+     }})
